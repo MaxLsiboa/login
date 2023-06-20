9e97d4c0432555ca704fd736e25b92d05bfbdd1b
MaxLsiboa <131786808+MaxLsiboa@users.noreply.github.com>
2023-06-20 19:45:16 -0300

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -61,7 +61,7 @@ def login():
                 else:
                     session['autenticado'] = True
                     session['usuario'] = usuario[1]  # Nome do usuário
-                    return redirect('/dashboard')
+                    return redirect('https://bot-bolsa.streamlit.app/')
             else:
                 # Caso as credenciais sejam inválidas, renderize a página de login novamente
                 return render_template('login.html', mensagem='Credenciais inválidas')
